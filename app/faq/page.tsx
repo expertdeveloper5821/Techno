eedb2057daa283be927f3aa42b972be2e062ec3e
expertdeveloper5821 <expert.developer.5821@gmail.com>
2026-04-21 11:08:24 +0530

    faq page is created

---
+++ app/faq/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '../components/faq-com/hero';
+import TopicsAccordion from '../components/faq-com/topics/TopicsAccordion';
+
+import Contact from '../components/faq-com/contact/contact';
+
+const FAQPage = () => {
+  return (
+    <>
+      <Hero />
+      <TopicsAccordion />
+      <Contact />
+      
+    </>
+  );
+};
+
+export default FAQPage;
